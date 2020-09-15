390418144db784b9f1496236dd419e92f5dbbb95
Ailton Peron Junior <junior.peron@hotmail.com>
2020-09-15 17:32:25 -0300

    cadastro

---
+++ atividade.js
@@ -0,0 +1,34 @@
+/* 
+Você deverá enviar um link do seu github com:
+
+* Formulário de Cadastro de clientes contendo
+- Nome
+- E-mail
+- CPF ou CNPJ
+- Telefone
+- CEP
+- Logradouro
+- Número
+- Bairro
+- Cidade
+- Estado
+
+* Listagem destes dados
+* Exclusão de algum item da lista
+
+Deverá ser feito em qualquer linguagem de programação, mas se você fizer isto em JavaScript, será considerado um diferencial.
+
+function validar(){
+   var nome = cadastrar.nome.value;
+   var email = cadastrar.email.value;
+   var cpf = cadastrar.cpf.value;
+   var tel = cadastrar.tel.value;
+   var cep = cadastrar.cep.value;
+   var logradouro = cadastrar.logradouro.value;
+   var num = cadastrar.num.value;
+   var bairro = cadastrar.bairro.value;
+   var cidade = cadastrar.cidade.value;
+   var estado = cadastrar.estado.value;
+}
+
+*/
