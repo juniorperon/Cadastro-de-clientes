f615b8c5eb7198c8112dfc4a61ac1606eb75553a
Ailton Peron Junior <junior.peron@hotmail.comgit config --global user.name JuniorPeron>
2021-01-03 20:31:00 -0300

    formulario completo com alert

--- atividade.js
+++ atividade.js
@@ -1,3 +1,29 @@
+function Enviar() {
+
+   var nome = document.getElementById("nomeid");
+   var email = document.getElementById("emailid");
+   var cpf = document.getElementById("cpfid");
+   var tel = document.getElementById("telid");
+   var cep = document.getElementById("cepid");
+   var end = document.getElementById("endid");
+   var num = document.getElementById("numid");
+   var bairro = document.getElementById("bairroid");
+   var cidade = document.getElementById("cidadeid");
+   var estado = document.getElementById("estadoid");
+
+       alert('Obrigado! ' + nome.value + ' os seus dados foram encaminhados com sucesso');
+       alert(`Seus dados foram: 
+                                 Email: ${email.value}
+                                 CPF: ${cpf.value} 
+                                 Telefone: ${tel.value}
+                                 CEP: ${cep.value} 
+                                 Endereço: ${end.value},${num.value}
+                                 Bairro: ${bairro.value}
+                                 Localização: ${cidade.value} - ${estado.value}`);
+
+
+}
+
 /* 
 Você deverá enviar um link do seu github com:
 
@@ -17,18 +43,5 @@ Você deverá enviar um link do seu github com:
 * Exclusão de algum item da lista
 
 Deverá ser feito em qualquer linguagem de programação, mas se você fizer isto em JavaScript, será considerado um diferencial.
-
-function validar(){
-   var nome = cadastrar.nome.value;
-   var email = cadastrar.email.value;
-   var cpf = cadastrar.cpf.value;
-   var tel = cadastrar.tel.value;
-   var cep = cadastrar.cep.value;
-   var logradouro = cadastrar.logradouro.value;
-   var num = cadastrar.num.value;
-   var bairro = cadastrar.bairro.value;
-   var cidade = cadastrar.cidade.value;
-   var estado = cadastrar.estado.value;
-}
 
 */
